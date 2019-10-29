ce044c37b651e4d7053d49c0f4a8fd2718719c51
alexmpereira <alexmpprog@gmail.com>
2019-10-29 12:19:38 -0300

    tratando alguns dados na serialização

--- app/Estudante.php
+++ app/Estudante.php
@@ -6,7 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estudante extends Model
 {
+    /**
+     * indica os atributos para definição de dados em massa
+     *
+     * @var array
+     */
     protected $fillable = ['nome', 'nascimento', 'sala_id'];
+
+    // /**
+    //  * Faz conversão na hora de serialização
+    //  *
+    //  * @var array
+    //  */
+    protected $casts = [
+        'nascimento' => 'date:d/m/Y'
+    ];
+
+    // /**
+    //  * Define atributos não mostrados depois da serialização
+    //  */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    // /**
+    //  * Define os atributos visiveis depois da serialização
+    //  *
+    //  * @var array
+    //  */
+    // protected $visible = ['nome', 'nascimento', 'sala_id'];
 
     /**
      * Mapeando de relacionamento da tabela Sala
