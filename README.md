d75bbbe3625ee3c6cb71884fb2300e4eb4fc55aa
alexmpereira <alexmpprog@gmail.com>
2019-10-31 12:49:26 -0300

    Implementando a ferramenta swagger para criação de documentação

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ Esqueleto com uma estrutura pronta para desenvolver API´s com Laravel
 - Comando para criar as validações de requests: **php artisan make:request EstudanteRequest**
 - Comando para criar resources (camada de transformação): **php artisan make:resource Estudante**
 - Comando para criar coleções de resources: **php artisan make:resource Estudantes --collection**
+
+## Swagger
+
+- Documentação: https://swagger.io/
+- Package usado no projeto: https://github.com/DarkaOnLine/L5-Swagger
+- No arquivo **.env** adicionar em algum lugar o parametro: **SWAGGER_VERSION=2.0**
+- Para gerar a documentação, digite o comando: **php artisan l5-swagger:generate**
+- Para acessar a documentação da API de estudante como teste: **localhost:8000/api/documentation
+
+## Documentos
+
+- Uso e instalação do CORS: https://github.com/barryvdh/laravel-cors
--- app/Http/Controllers/Painel/EstudanteController.php
+++ app/Http/Controllers/Painel/EstudanteController.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * @SWG\Swagger(
+ *     basePath="/api",
+ *     schemes={"http"},
+ *     host="http://localhost:8000",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="API Estudantes",
+ *         description="API de Estudantes",
+ *         @SWG\Contact(
+ *             email="alexmpprog@gmail.com"
+ *         ),
+ *     )
+ * )
+ */
+
 namespace App\Http\Controllers\Painel;
 
 use Illuminate\Http\Request;
@@ -13,6 +29,26 @@ use App\Http\Resources\Estudantes as EstudanteCollection;
 
 class EstudanteController extends Controller
 {
+    /**
+     * @SWG\Get(
+     *      path="/estudantes",
+     *      operationId="getestudantesList",
+     *      tags={"estudantes"},
+     *      summary="Get list of estudantes",
+     *      description="Returns list of estudantes",
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     *       @SWG\Response(response=400, description="Bad request"),
+     *       security={
+     *           {"api_key_security_example": {}}
+     *       }
+     *     )
+     *
+     * Returns list of estudantes
+     */
+
     /**
      * Display a listing of the resource.
      *
@@ -35,6 +71,28 @@ class EstudanteController extends Controller
     {
         return Estudante::create($request->all());
     }
+
+    /**
+     * @SWG\Get(
+     *      path="/estudantes/{id}",
+     *      operationId="getEstudanteById",
+     *      tags={"estudantes"},
+     *      summary="Informação de um estudante",
+     *      description="Retorna um único estudante",
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *       ),
+     *      @SWG\Response(response=400, description="Bad request"),
+     *      @SWG\Response(response=404, description="Resource Not Found"),
+     *      security={
+     *         {
+     *             "oauth2_security_example": {"write:estudantes", "read:estudantes"}
+     *         }
+     *     },
+     * )
+     *
+     */
 
     /**
      * Display the specified resource.
