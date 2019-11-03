acd1877dd059bb71a5ce5700fbb6fe48489f2c75
alexmpereira <alexmpprog@gmail.com>
2019-11-03 09:04:57 -0300

    Ultimos ajustes e testes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,9 +50,9 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if( $request->is('api/*') )
-        // {
-        //     return $this->getJsonException( $request, $exception );
-        // }
+        {
+            return $this->getJsonException( $request, $exception );
+        }
         return parent::render($request, $exception);
     }
 }
